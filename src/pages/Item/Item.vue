9da4940f8db354558796c9cc3d9c37e9ac1296d5
xuanwushijie <1091237632@qq.com>
2018-07-25 14:08:52 +0800

    item静态页面

--- src/pages/Item/Item.vue
+++ src/pages/Item/Item.vue
@@ -1,11 +1,308 @@
 <template>
   <div>
-    Item
+    <div class="item-header">
+      <div class="wrap">
+        <i class="icon"></i>
+        <span class="serach-content">搜索商品, 共10918款好物</span>
+      </div>
+    </div>
+    <div class="item-left">
+      <div class="main-left">
+        <ul class="list">
+          <li class="item active"><a href="" class="txt">推荐专区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+          <li class="item"><a href="" class="txt">爆品区</a></li>
+        </ul>
+      </div>
+
+    </div>
+    <div class="main-right">
+      <div class="right-top">
+        <img src="./images/banner.jpg" alt="">
+      </div>
+      <div class="right-bottom">
+        <div class="hd">
+          <span class="txt">
+            <span class="txt1">推荐专区</span>
+            <span class="txt2">分类</span>
+          </span>
+        </div>
+        <ul class="list">
+          <li class="item">
+            <a href="">
+              <div class="lazy-img">
+                <img
+                  src="http://yanxuan.nosdn.127.net/0251e44f3016055785bdb64801e4d32c.jpg?imageView&quality=85&thumbnail=144x144"
+                  alt="">
+              </div>
+              <div class="name">配件服饰特惠</div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="">
+              <div class="lazy-img">
+                <img
+                  src="http://yanxuan.nosdn.127.net/0251e44f3016055785bdb64801e4d32c.jpg?imageView&quality=85&thumbnail=144x144"
+                  alt="">
+              </div>
+              <div class="name">配件服饰特惠</div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="">
+              <div class="lazy-img">
+                <img
+                  src="http://yanxuan.nosdn.127.net/0251e44f3016055785bdb64801e4d32c.jpg?imageView&quality=85&thumbnail=144x144"
+                  alt="">
+              </div>
+              <div class="name">配件服饰特惠</div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="">
+              <div class="lazy-img">
+                <img
+                  src="http://yanxuan.nosdn.127.net/0251e44f3016055785bdb64801e4d32c.jpg?imageView&quality=85&thumbnail=144x144"
+                  alt="">
+              </div>
+              <div class="name">配件服饰特惠</div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="">
+              <div class="lazy-img">
+                <img
+                  src="http://yanxuan.nosdn.127.net/0251e44f3016055785bdb64801e4d32c.jpg?imageView&quality=85&thumbnail=144x144"
+                  alt="">
+              </div>
+              <div class="name">配件服饰特惠</div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="">
+              <div class="lazy-img">
+                <img
+                  src="http://yanxuan.nosdn.127.net/0251e44f3016055785bdb64801e4d32c.jpg?imageView&quality=85&thumbnail=144x144"
+                  alt="">
+              </div>
+              <div class="name">配件服饰特惠</div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="">
+              <div class="lazy-img">
+                <img
+                  src="http://yanxuan.nosdn.127.net/0251e44f3016055785bdb64801e4d32c.jpg?imageView&quality=85&thumbnail=144x144"
+                  alt="">
+              </div>
+              <div class="name">配件服饰特惠</div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="">
+              <div class="lazy-img">
+                <img
+                  src="http://yanxuan.nosdn.127.net/0251e44f3016055785bdb64801e4d32c.jpg?imageView&quality=85&thumbnail=144x144"
+                  alt="">
+              </div>
+              <div class="name">配件服饰特惠</div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="">
+              <div class="lazy-img">
+                <img
+                  src="http://yanxuan.nosdn.127.net/0251e44f3016055785bdb64801e4d32c.jpg?imageView&quality=85&thumbnail=144x144"
+                  alt="">
+              </div>
+              <div class="name">配件服饰特惠</div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="">
+              <div class="lazy-img">
+                <img
+                  src="http://yanxuan.nosdn.127.net/0251e44f3016055785bdb64801e4d32c.jpg?imageView&quality=85&thumbnail=144x144"
+                  alt="">
+              </div>
+              <div class="name">配件服饰特惠</div>
+            </a>
+          </li>
+          <li class="item">
+            <a href="">
+              <div class="lazy-img">
+                <img
+                  src="http://yanxuan.nosdn.127.net/0251e44f3016055785bdb64801e4d32c.jpg?imageView&quality=85&thumbnail=144x144"
+                  alt="">
+              </div>
+              <div class="name">配件服饰特惠</div>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 <script>
   export default {}
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixins.styl"
+  .item-header
+    position fixed !important
+    z-index 100
+    left 0
+    top 0
+    width 100%
+    background #fff
+    height (87 /$rem)
+    border-bottom 1px solid #ededed
+    .wrap
+      margin (16 /$rem 30 /$rem)
+      display flex
+      align-items center
+      flex-grow 1
+      justify-content center
+      height: (56 /$rem)
+      font-size: (28 /$rem)
+      background-color: #ededed;
+      border-radius: (8 /$rem)
+      .icon
+        display inline-block
+        vertical-align middle
+        background-image url("./images/search.png")
+        background-repeat no-repeat
+        background-size (100% 100%)
+        width (28 /$rem)
+        height (28 /$rem)
+        margin-right (10 /$rem)
+      .serach-content
+        color #666
+        display block
+        font-size: (28 /$rem)
 
+  .item-left
+    position fixed
+    left 0
+    top (88 /$rem)
+    width (162 /$rem)
+    overflow hidden
+    background-color #fff
+    height 100%
+    .main-left
+      position absolute
+      width 100%
+      height 95%
+      .list
+        border-right solid 1px #ededed
+        padding (40 /$rem 0 138 /$rem)
+        .item
+          margin-top (40 /$rem)
+          height (50 /$rem)
+          width 100%
+          text-align center
+          &:first-child
+            margin-top 0
+          &.active
+            position relative
+            &:before
+              content ''
+              position absolute
+              top 0
+              left 0
+              bottom 0
+              width (6 /$rem)
+              background-color #ab2b2b
+            .txt
+              font-size (36 /$rem)
+              color #ab2b2b
+          .txt
+            display block
+            font-size (28 /$rem)
+            line-height (50 /$rem)
+            white-space nowrap
+            overflow hidden
+            text-overflow ellipsis
+            color #333
+
+  .main-right
+    position fixed
+    left 0
+    top (88 /$rem)
+    width (588 /$rem)
+    background-color #fff
+    height 100%
+    margin-left (162 /$rem)
+    padding (30 /$rem 30 /$rem 21 /$rem)
+    overflow hidden
+    .right-top
+      background center no-repeat #f4f4f4
+      width 100%
+      height (192 /$rem)
+      background-size cover
+      border-radius (4 /$rem)
+      img
+        width (528 /$rem)
+        height (192 /$rem)
+    .right-bottom
+      .hd
+        font-size (24 /$rem)
+        height (108 /$rem)
+        color #333
+        display flex
+        align-items center
+        justify-content center
+        .txt
+          display flex
+          align-items center
+          justify-content center
+          padding (0 8 /$rem)
+          color #7f7f7f
+          line-height (30 /$rem)
+          font-size (24 /$rem)
+          &::before, &::after
+            content ''
+            display inline-block
+            width (24 /$rem)
+            height 1px
+            background-color #7f7f7f
+          .txt1
+            margin-left (8 /$rem)
+          .txt2
+            margin-right (8 /$rem)
+
+      .list
+        .item
+          display inline-block
+          margin-right (7 /$rem)
+          width (144 /$rem)
+          font-size 0
+          vertical-align top //为了lazy-img
+          &:nth-child(3n)
+            margin-right (-10 /$rem)
+          a
+            .lazy-img
+              width (144 /$rem)
+              height (144 /$rem)
+              img
+                width 100%
+                height 100%
+            .name
+              text-align center
+              color #333
+              line-height (34 /$rem)
+              font-size (24 /$rem)
 </style>
